da043fe745202aece0cde76725285656b0ea7796
roliauo <roliauo@gmail.com>
2022-07-12 15:36:40 +0800

    [fix] loading images

--- src/shop/views/Home.tsx
+++ src/shop/views/Home.tsx
@@ -3,13 +3,13 @@ import styled from "styled-components";
 import './root.css';
 import Layout from "./components/Layout";
 import Slideshow from "./components/Slideshow";
-import { SLIDESHOWS_BANNER } from "../constants";
+import { SLIDESHOWS_BANNER_URL } from "../constants";
 
 
 export default function View() {
     return (
         <Layout>
-            <Slideshow slideshows={SLIDESHOWS_BANNER}/>
+            <Slideshow slideshows={SLIDESHOWS_BANNER_URL}/>
         </Layout>
     )  
 }
